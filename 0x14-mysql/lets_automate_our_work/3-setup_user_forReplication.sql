3d82a22a8a95ac64eed4a28ac8ff3fd69deb4e8f
attahchisom6 <attahchisom6@gmail.com>
2023-04-19 18:43:58 +0100

    made a change

--- 0x14-mysql/lets_automate_our_work/3-setup_user_forReplication.sql
+++ 0x14-mysql/lets_automate_our_work/3-setup_user_forReplication.sql
@@ -1,7 +1,7 @@
 -- we will set up a user for our replication server and grant it a slave replication privilege
 -- This script should be run on your master server
 
--- Notee: any password can do herw
+-- Notee: any password can do here
 CREATE USER IF NOT exists 'replica_user'@'%' IDENTIFIED BY 'web';
 GRANT REPLICATION SLAVE ON * . * TO 'replica_user'@'%';
 GRANT SELECT ON mysql . user TO 'holberton_user'@'localhost';
