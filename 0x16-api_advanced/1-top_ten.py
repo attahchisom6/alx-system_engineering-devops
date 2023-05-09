ee29ff9534cd48cec984b07e20076a2e0e326b39
attahchisom6 <attahchisom6@gmail.com>
2023-05-09 17:56:07 +0000

    top ten titles

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+gets the hottest top ten title in a querry
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """function to get the top ten hottest title
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        j_response = response.json().get('data')
+
+        ten_titles = j_response.get('children')[:10]
+        for post in ten_titles:
+            print(post.get('data').get('title'))
+    print(None)
