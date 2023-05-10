bbefd98adfff447842ca0b486c91b448507d389b
attahchisom6 <attahchisom6@gmail.com>
2023-05-10 16:33:49 +0000

    found and debugged it

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-#A puppet manifest to automate the and fix error from avqurried server
+# Puppet manifest to fix a bug in wp-setings.php
 
-exec {'To Fix the issue i found':
-	command	=> 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path	=> '/usr/local/bin/:/bin/'
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
