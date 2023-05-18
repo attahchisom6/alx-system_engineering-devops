db54ad3ce2be6ac9e4a3f272c790fac763e62fc4
attahchisom6 <attahchisom6@gmail.com>
2023-05-18 17:08:09 +0100

    increasing nginx request limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,12 +5,13 @@ exec {'fix-nginx':
 	provider => 'shell',
 	command	=> 'sed -i "s/15/4096/" /etc/default/nginx',
 
-	path	=> '/usr/local/bin:/bin/',
+	path	=> '/usr/local/bin/:/bin/',
 	before	=> Exec['restarting nginx'],
 }
 
 # restart nginx
 exec {'restarting nginx':
 	provider	=> 'shell',
-	command => 'sudo service nginx restart',
+	command => 'nginx restart',
+	path	=> '/etc/init.d',
 }
