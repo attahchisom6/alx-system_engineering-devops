07e6912b96b1ab36fd4849d41132319e36ac5972
attahchisom6 <attahchisom6@gmail.com>
2023-05-18 17:10:45 +0100

    increasing nginx request limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 # A puppet manifest to increaase the value of the request nginx handles
 
-# increase value of ulimit
 exec {'fix-nginx':
 	provider => 'shell',
 	command	=> 'sed -i "s/15/4096/" /etc/default/nginx',
