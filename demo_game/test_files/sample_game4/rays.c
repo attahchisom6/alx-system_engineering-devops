774ff158302d3650ccf415bb7f220de07966ca10
attahchisom6 <attahchisom@gmail.com>
2023-05-27 03:50:01 +0100

    commit

--- demo_game/test_files/sample_game4/rays.c
+++ demo_game/test_files/sample_game4/rays.c
@@ -3,7 +3,8 @@
 ray_t rays[NUM_RAYS];
 
 static bool foundHorzWallHit = false, foundVertWallHit = false;
-static float horzWallHitX, horzWallHitY, HorzWallContent, vertWallHitX, vertWallHitY;
+static float horzWallHitX, horzWallHitY, HorzWallContent;
+static float vertWallHitX, vertWallHitY, vertWallContent;
 /**
  * HorzIntersection - find horizontal intersection on a wall
  * @rayAngle: current ray angle
@@ -15,7 +16,7 @@ void HorzIntersection(float rayAngle)
 {
 	float Ystep, Xstep, Yintercept, Xintercept, nextHorzHitX, nextHorzHitY;
 
-	foundHorzWallhit = false;
+	foundHorzWallHit = false;
 	HorzWallHitX = HorzWallHitY = HorzWallContent = 0;
 
 	Yintercept = floor(player.y / titleSize) * titleSize;
@@ -34,6 +35,7 @@ void HorzIntersection(float rayAngle)
 	while (isInsideMap(nextHorzHitX, nextHorzHitY))
 	{
 		float Ytest = nextHorzHitY, Xtest = nextHorzHitY;
++
 
 		if (detectCollision(Xtest, Ytest))
 		{
@@ -50,3 +52,23 @@ void HorzIntersection(float rayAngle)
 		nextHorzWallHitY = Ystep;
 	}
 }
+
+/**
+ * VertIntercept - checks and calculates for vartical interception
+ *
+ * Return: void
+ */
+
+void VertIntercept(float rayAngle)
+{
+	float Xstep, Ystep, Xintercept, Yintercept, nextVertHitX, nextVertHitY;
+
+	vertHitWallX = vertHitWallY = vertWallContent = 0;
+
+	Xintercept = floor(player.x * titleSize) / titleSize
+	Xintercept += isRayFacingRight(rayAngle) ? titleSizei : 0;
+	Yintercept = player.y + (Xintercept - player.x) * tan(rayAngle);
+
+	Xstep = titleSIze;
+	Xstep *= isRayFacingLeft(rayAngle) ? -1 : 1;
+	Ystep = 
