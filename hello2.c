1c3269ee015e6cd7306c7b76b3841bd168585bf5
attahchisom6 <attahchisom6@gmail.com>
2023-05-28 08:10:19 +0000

    adding

---
+++ hello2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COLS 5
+#define ROWS 9
+
+int main(void)
+{
+	int k, p;
+	char *map = malloc(ROWS * COLS * sizeof(char));
+	for (p = 0; p < ROWS; p++)
+	{
+		for (k = 0; k < COLS; k++)
+		{
+			map[p + k * COLS] = 5;
+		}
+	}
+
+	for (p = 0; p < ROWS; p++)
+	{
+		for (k = 0; k < COLS; k++)
+		{
+			printf("%d ", map[p + k * COLS]);
+		}
+		printf("\n");
+	}
+	free(map);
+}
