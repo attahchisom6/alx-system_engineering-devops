b0d94eb3dc652d74a42f40e1ba59fe82a1df8a39
attahchisom6 <attahchisom6@gmail.com>
2023-06-02 20:59:48 +0000

    adding

--- demo_game/test_files/sample_game4/rayMetrics.c
+++ demo_game/test_files/sample_game4/rayMetrics.c
@@ -1,8 +1,8 @@
 #include "game.h"
-#include <math.h>
 
 /**
- * EuclideanDistance - get the pythogoreanean/Euclidean distance between two points
+ * EuclideanDistance - get the pythogoreanean/Euclidean distance
+ * between two points
  * @x0: inital x coordinate
  * @y0: initial y coordinate
  * @x1: displaced x coordinate
@@ -21,8 +21,54 @@ float EuclideanDistance(float x0, float y0, float x1, float y1)
 	return (distance);
 }
 
-int main(void)
+/**
+ * isRayFacingDown - checks if the rays is facing down
+ * @rayAngle: ray Angle
+ *
+ * Return: true if ray is facing down else false
+ */
+
+bool isRayFacingDown(float rayAngle)
 {
-	printf("%f\n", EuclideanDistance(0, 5, 5, 0));
-	return 0;
+	if (rayAngle > 0 && rayAngle < PI)
+		return (true);
+	return (false);
+}
+
+/**
+ * isRayFacingUp - check if the ray is facing up
+ * @rayAngle: ray Angle
+ *
+ * Return: true ic ray is facing up else false
+ */
+
+bool isRayFacingUp(float rayAngle)
+{
+	return (!isRayFacingDown(rayAngle));
+}
+
+/**
+ * isRayFacingRight - checks if the ray is facing right
+ * @rayAngle: rayAngle
+ *
+ * Return: true if ray is facing right, else false
+ */
+
+bool isRayFacingRight(float rayAngle)
+{
+	if (rayAngle < (PI / 2) || rayAngle > TWO_PI / 3)
+		return (true);
+	return (false);
+}
+
+/**
+ * isRayFacingLeft - checks if the ray os facing left
+ * @rayAngle: angle of the ray
+ *
+ * Return: true if ray is facing left
+ */
+
+bool isRayFacingLeft(float rayAngle)
+{
+	return (!isRayFacingRight(rayAngle));
 }
